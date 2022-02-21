3047387f0de575071c39f82619bc86ff7a0ccc61
Laura Train <60850007+lauratrain99@users.noreply.github.com>
2022-02-21 13:38:49 +0100

    function to propagate

--- estimation/propagateOrbit.m
+++ estimation/propagateOrbit.m
@@ -13,11 +13,12 @@ function [propagate] = propagateOrbit(init_integ, final_integ, TLE)
     options = odeset('RelTol',1e-8,'AbsTol',1e-8); 
     
     [t,y] = ode45(@dragDyn,linspace(t0, tf,1000), iniState, options, TLE.Bc);  
-
+    
+    propagate.t = t;
     propagate.r_ECI = y(:,1:3);
     propagate.v_ECI = y(:,4:6);
 
     % Compute ground trajectory
-    [propagate.utc_prop] = integ_t2utc(t);
-    [propagate.lat, propagate.lon, propagate.alt] = ground_trajectory(propagate.r_ECI, propagate.utc_prop);
+    [propagate.timeUTC] = integ_t2utc(t);
+    [propagate.lat, propagate.lon, propagate.alt] = ground_trajectory(propagate.r_ECI, propagate.timeUTC);
 end
